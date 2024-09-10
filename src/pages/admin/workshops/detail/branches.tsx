654bc3731354be61cd151e3b8fa6168cd8699b54
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2024-09-10 15:19:56 +0500

    builded

--- src/pages/admin/workshops/detail/branches.tsx
+++ src/pages/admin/workshops/detail/branches.tsx
@@ -1,12 +1,11 @@
 import { PageHeader } from '@/components';
-import { Fragment } from 'react/jsx-runtime';
 import { WorkshopBranchesTable } from './tabel/table';
 
 export const WorkshopBrnches = () => {
     return (
-        <Fragment>
+        <>
             <PageHeader title="Цеҳ бўлимлари" />
             <WorkshopBranchesTable />
-        </Fragment>
+        </>
     );
 };
