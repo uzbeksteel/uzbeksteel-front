78c3ea3f602e2df62ad8d526ce487396fd545a4d
rakhimov <shukurulloh.com@gmail.com>
2024-10-17 19:02:05 +0500

    add user table

--- src/pages/workshop/employes/exmployes.tsx
+++ src/pages/workshop/employes/exmployes.tsx
@@ -1,20 +1,11 @@
-import { Box, Card, Icon, Table, Typography } from '@/components';
-import { useNavigate } from 'react-router-dom';
-import { Fragment } from 'react/jsx-runtime';
+import { PageHeader } from '@/components';
+import { WorkshopEmployeeTable } from './table';
 
 export const WorkshopEmployes = () => {
-    const navigate = useNavigate();
     return (
-        <Fragment>
-            <Box $p="20px" $gap="10px" style={{ background: '#FFF' }}>
-                <Icon onClick={() => navigate(-1)} style={{ cursor: 'pointer' }} name="ArrowLeft" />
-                <Typography type="title" level={3}>
-                    Ходимлар
-                </Typography>
-            </Box>
-            <Card>
-                <Table scroll={{ x: true }} columns={[]} dataSource={[]} titleTable="Ходимлар рўйхати" />
-            </Card>
-        </Fragment>
+        <>
+            <PageHeader title="Ходимлар" />
+            <WorkshopEmployeeTable />
+        </>
     );
 };
