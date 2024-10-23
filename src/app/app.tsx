b0f9ec02c3e05b9e74a637fc3345ebf23784eb13
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2024-10-24 00:28:46 +0500

    update notifications

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,8 +2,8 @@ import { Modal } from '@/components';
 import { useToken } from '@/lib/hooks';
 import { history } from '@/lib/utils';
 import { Router } from '@/router';
-import { HistoryRouterProvider, QueryProvider, ThemeProvider } from './providers';
 import { Suspense } from 'react';
+import { HistoryRouterProvider, QueryProvider, ThemeProvider } from './providers';
 
 export const App = () => {
     const { isInitiated } = useToken();
